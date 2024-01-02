024890d5e0748e2ff7c74b57c4876251d526aab1
Yufccode <2573312458@qq.com>
2024-01-02 23:20:29 +0800

    update20240102

--- stl_vector/stl_vector.hpp
+++ stl_vector/stl_vector.hpp
@@ -5,6 +5,7 @@
 #include <string.h>
 #include <assert.h>
 #include <iostream>
+#include <initializer_list>
 
 namespace BagPack
 {
@@ -13,8 +14,11 @@ namespace BagPack
     {
     public:
         typedef T *iterator;
+        const typedef T *const_iterator;
         iterator begin() { return __start; }
         iterator end() { return __finish; }
+        const_iterator begin() const { return __start; }
+        const_iterator end() const { return __finish; }
 
     private:
         iterator __start;
@@ -28,6 +32,60 @@ namespace BagPack
         {
             delete[] __start;
             __start = __finish = __end_of_storage = nullptr;
+        }
+        // 支持迭代器区间的构造
+        template <class InputIterator>
+        vector(InputIterator first, InputIterator last)
+            : __start(nullptr), __finish(nullptr), __end_of_storage(nullptr)
+        {
+            while (first != last)
+            {
+                push_back(*first++); // 直接push_back会报错, 要记得初始化
+            }
+        }
+        // 支持初始化列表构造
+        vector(const std::initializer_list<T> &in_lst)
+            : __start(nullptr), __finish(nullptr), __end_of_storage(nullptr)
+        {
+            typename std::initializer_list<T>::iterator iter = in_lst.begin();
+            reserve(in_lst.size());
+            while (iter != in_lst.end())
+            {
+                push_back(*iter++);
+            }
+        }
+        // 支持n个同样的东西构造
+        vector(size_t n, const T &val = T())
+            : __start(nullptr), __finish(nullptr), __end_of_storage(nullptr)
+        {
+            /*
+                注意：用这个进行构造，有可能会调用到 迭代器区间构造的构造函数里面去
+                两个类型都是int的时候就会出错，因为他会认为迭代器构造那个构造更匹配
+            */
+            reserve(n);
+            for (size_t i = 0; i < n; i++)
+            {
+                push_back(val);
+            }
+        }
+        // 拷贝构造
+        vector(const vector<T> &v)
+            : __start(nullptr), __finish(nullptr), __end_of_storage(nullptr)
+        {
+            vector<T> tmp(v.begin(), v.end());
+            swap(tmp); // this 和 tmp swap一下
+        }
+        vector<T> &operator=(const vector<T> &v)
+        {
+            vector<T> tmp(v.begin(), v.end());
+            swap(tmp); // this 和 tmp swap一下
+            return *this;
+        }
+        void swap(vector<T> &v)
+        {
+            std::swap(__start, v.__start);
+            std::swap(__finish, v.__finish);
+            std::swap(__end_of_storage, v.__end_of_storage);
         }
 
     public:
@@ -48,12 +106,41 @@ namespace BagPack
                 if (__start)
                 {
                     // 如果旧空间不是空，就要把旧的内容拷贝到新空间上去
-                    memcpy(tmp, __start, sizeof(T) * sz);
+                    // memcpy(tmp, __start, sizeof(T) * sz); // memcpy是有问题的
+                    /*
+                        memcpy是有问题的，因为memcpy不会触发自定义类型的拷贝构造！
+                        所以如果我们的vector里面存的是内置问题，那是没问题的
+                        但是如果我们是自定义类型的时候，memcpy就拷贝不过去了
+                    */
+                    for (size_t i = 0; i < sz; i++)
+                    {
+                        tmp[i] = __start[i];
+                    }
                     delete[] __start; // 释放旧的空间
                 }
                 __start = tmp; // 让start指向新的空间
                 __finish = __start + sz;
                 __end_of_storage = __start + n;
+            }
+        }
+        void resize(size_t n, const T &val = T())
+        {
+            if (n > capacity())
+            {
+                reserve(n);
+            }
+            if (n > size())
+            {
+                // 初始化填值
+                while (__finish < __start + n)
+                {
+                    *__finish = val;
+                    ++__finish;
+                }
+            }
+            else
+            {
+                __finish = __start + n;
             }
         }
 
@@ -68,6 +155,16 @@ namespace BagPack
             assert(pos < size());
             return __start[pos];
         }
+        T &front()
+        {
+            assert(size() > 0);
+            return *__start;
+        }
+        T &back()
+        {
+            assert(size() > 0);
+            return *(__finish - 1);
+        }
 
     public:
         void push_back(const T &x)
@@ -78,6 +175,47 @@ namespace BagPack
             }
             *__finish = x;
             ++__finish; // 注意，__finish是最后一个位置的下一个位置
+        }
+        void pop_back()
+        {
+            assert(__finish > __start);
+            --__finish;
+        }
+        iterator insert(iterator pos, const T &x)
+        {
+            assert(pos >= __start && pos <= __finish);
+            if (__finish == __end_of_storage)
+            {
+                size_t length = pos - __start;
+                reserve(capacity() == 0 ? 4 : capacity() * 2); // 扩容
+                pos = __start + length;                        // 用这两句解决迭代器失效问题！
+            }
+            // 挪动数据
+            iterator end = __finish - 1;
+            while (end >= pos)
+            {
+                *(end + 1) = *end;
+                --end;
+            }
+            *pos = x;
+            ++__finish;
+            return pos;
+        }
+        iterator erase(iterator pos)
+        {
+            /*
+                erase迭代器会失效吗？我们实现这个是不会的，因为我们没动空间
+                库里会不会呢？库里如果缩容，就会有！
+            */
+            assert(pos >= __start && pos < __finish);
+            iterator begin = pos + 1;
+            while (begin < __finish)
+            {
+                *(begin - 1) = *begin;
+                ++begin;
+            }
+            --__finish;
+            return pos;
         }
     };
 } // namespace BagPack
--- stl_vector/test.cc
+++ stl_vector/test.cc
@@ -1,5 +1,7 @@
 
 #include "stl_vector.hpp"
+#include <algorithm>
+#include <list>
 
 void test1()
 {
@@ -28,9 +30,237 @@ void test1()
     }
     std::cout << std::endl;
 }
+void test2_func(const BagPack::vector<int> &v)
+{
+    // 如果没写拷贝，就要记得带上 &，不然就会出问题
+    BagPack::vector<int>::const_iterator it = v.begin();
+    while (it != v.end())
+    {
+        std::cout << *it << " ";
+        ++it;
+    }
+    std::cout << std::endl;
+}
+void test2()
+{
+    BagPack::vector<int> arr;
+    arr.push_back(1);
+    arr.push_back(2);
+    arr.push_back(3);
+    test2_func(arr);
+}
+void test3()
+{
+    BagPack::vector<int> v;
+    v.push_back(1);
+    v.push_back(2);
+    v.push_back(3);
+    v.push_back(4);
+    // v.push_back(5); // 有这一行就能跑通，如果没有就跑不通？为什么？
 
+    // 遍历
+    for (const auto &e : v)
+    {
+        std::cout << e << " ";
+    }
+    std::cout << std::endl;
+
+    auto p = std::find(v.begin(), v.end(), 3);
+    if (p != v.end())
+    {
+        v.insert(p, 66); // 在3的前面插入一个30
+        /*
+            这个insert可能会发生扩容！
+            扩完容，这个p还有效吗？
+            vector的空间已经到别的地方去了，你的p还在原地
+            那p真永远都不可能等于 v.end() 了
+            因为 v.end() 已经到别的地方去了
+            这个就是迭代器失效问题！
+        */
+        // ok 我们在insert里面更新了pos，那insert完就可以用这个p了吗？
+        // 可以 继续 cout << *p; 或者 v.insert(p, 55); 这样吗？
+        // 不可以！insert里面修改的pos和外面的p有关系吗？没关系！形式参数不影响实际参数！
+        // 为什么不把参数改成引用，让它浅拷贝呢？不要，因为库里面就是深拷贝，库这样实现是有它自己的原因的
+        // 一个简单的例子，v.insert(v.begin(), 0); 如果改成引用还得了？
+        // 首先v.begin()是肯定不能改的，也不给改！如果参数改成引用 v.insert(v.begin(), 0);
+        // 肯定是编译不通过的，因为v.begin()这个函数返回迭代器，是传值返回，会发生拷贝，产生
+        // 临时对象，临时对象具有常性
+        // 记住，insert/erase后，这个迭代器，不要再用了！！
+    }
+
+    // 遍历
+    for (const auto &e : v)
+    {
+        std::cout << e << " ";
+    }
+    std::cout << std::endl;
+}
+void test4()
+{
+    BagPack::vector<int> v;
+    v.push_back(1);
+    v.push_back(2);
+    v.push_back(3);
+    v.push_back(4);
+    v.push_back(5);
+
+    for (const auto &e : v)
+    {
+        std::cout << e << " ";
+    }
+    std::cout << std::endl;
+
+    BagPack::vector<int>::iterator it_p = std::find(v.begin(), v.end(), 3);
+    v.erase(it_p);
+
+    for (const auto &e : v)
+    {
+        std::cout << e << " ";
+    }
+    std::cout << std::endl;
+}
+void test5()
+{
+    BagPack::vector<int> v;
+    v.push_back(1);
+    v.push_back(2);
+    v.push_back(2);
+    v.push_back(3);
+    v.push_back(4);
+    v.push_back(5);
+    v.push_back(6);
+
+    // 删除所有的偶数 -- 这里如果处理不当也会遇到失效问题
+    for (const auto &e : v)
+    {
+        std::cout << e << " ";
+    }
+    std::cout << std::endl;
+
+    BagPack::vector<int>::iterator iter = v.begin();
+    while (iter != v.end())
+    {
+        if (*iter % 2 == 0)
+        {
+            iter = v.erase(iter); // 更新一下迭代器
+        }
+        else
+        {
+            iter++;
+        }
+    }
+
+    for (const auto &e : v)
+    {
+        std::cout << e << " ";
+    }
+    std::cout << std::endl;
+}
+void test6()
+{
+    // 把深拷贝完善一下
+    // 迭代器区间的构造
+    std::list<int> lst = {1, 3, 5, 7, 9};
+    BagPack::vector<int> v(lst.begin(), lst.end());
+    for (const auto &e : v)
+    {
+        std::cout << e << " ";
+    }
+    std::cout << std::endl;
+
+    BagPack::vector<int> v2;
+    v2 = v;
+    for (const auto &e : v2)
+    {
+        std::cout << e << " ";
+    }
+    std::cout << std::endl;
+}
+void test7()
+{
+    // 初始化列表初始化
+    BagPack::vector<int> arr = {1, 3, 5, 7, 9, 11};
+    for (const auto &e : arr)
+    {
+        std::cout << e << " ";
+    }
+    std::cout << std::endl;
+}
+void test8()
+{
+    BagPack::vector<std::string> arr = {"aa", "bb", "cc"};
+    for (const auto &e : arr)
+    {
+        std::cout << e << " ";
+    }
+    std::cout << std::endl;
+}
+void test9()
+{
+    BagPack::vector<std::string> arr(3, "aaa");
+    for (const auto &e : arr)
+    {
+        std::cout << e << " ";
+    }
+    std::cout << std::endl;
+}
+void test10()
+{
+    // 测试resize
+    BagPack::vector<int> v;
+    v.resize(10, 0);
+    for (const auto e : v)
+    {
+        std::cout << e << " ";
+    }
+    std::cout << std::endl;
+
+    BagPack::vector<int> v1 = {1, 2, 3, 4, 5};
+    v1.resize(10, 0);
+    for (const auto e : v1)
+    {
+        std::cout << e << " ";
+    }
+    std::cout << std::endl;
+
+    v1.resize(20, 20);
+    for (const auto e : v1)
+    {
+        std::cout << e << " ";
+    }
+    std::cout << std::endl;
+
+    v1.resize(3);
+    for (const auto e : v1)
+    {
+        std::cout << e << " ";
+    }
+    std::cout << std::endl;
+}
+BagPack::vector<BagPack::vector<int>> test11()
+{
+    // 写一个vector的最后一个要解决的大问题
+    BagPack::vector<BagPack::vector<int>> vv;
+    vv.push_back({1, 2, 3, 4});
+    vv.push_back({1, 2, 3, 4});
+    vv.push_back({1, 2, 3});
+    vv.push_back({1, 2, 3});
+    vv.push_back({1, 2, 3});
+    vv.push_back({1, 2, 3});
+    vv.push_back({1, 2, 3});
+    return vv; // 传值返回就有问题了！
+    // 因为里面变成一个浅拷贝了
+}
 int main()
 {
-    test1();
+    BagPack::vector<BagPack::vector<int>> vv = test11();
+    for (int i = 0; i < vv.size(); i++)
+    {
+        for (int j = 0; j < vv[i].size(); j++)
+        {
+            std::cout << vv[i][j] << " ";
+        }
+        std::cout << "\n";
+    }
     return 0;
 }
